d92cf6a08f0bd2001b83f8ea6b663c9a95c00a2a
Kareem Mahfouz <kareemmahfouz01@gmail.com>
2025-06-23 22:34:29 +0300

    -Refactored folder structure for improved readability and maintainability -Flutter native splash added for android 12, 12 & below and Ios

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:laza/core/helpers/assets.dart';
 
 class LazaApp extends StatelessWidget {
   const LazaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Laza App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Laza Home Page')),
+        body: Container(
+          color: Colors.green,
+          child: Center(child: SvgPicture.asset(Assets.lazaLogoSvg)),
+        ),
+      ),
+    );
   }
 }
